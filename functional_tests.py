bd376fdc99814f78179cfceede98cc5678a135d6
Matt Titterington <Matt Titterington>
2015-12-01 21:20:25 +0000

    Embellished functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,16 +16,31 @@ class NewVisitorTest(unittest.TestCase):
 
 		#Do the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		#Can you enter a to-do item straight away?
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			input.get_attribute('placeholder'), 
+			'Enter a to-do item'
+		)
 
 		#Someone wants to "Buy Peacock feathers"
+		inputbox.send_keys("Buy peacock feathers")
 
 		#When you hit enter the page updates and lists "1: Buy peacock feathers"
-
+		input.send_keys(Keys.ENTER)
+		
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == "1: Buy peacock feathers" for row in rows)
+		)
+		
 		#There should still be a text box to enter another task such as "Use peacock feathers to make a fly"
-
+		self.fail("finish da test")
+		
 		#The page updates again with both items on the list
 
 		#The page should generate a unique URL so teh list can be saved - with explanation
