ad68fa08b4b7e44533ec19462083328c1621c880
Matt Titterington <Matt Titterington>
2015-12-28 21:33:06 +0000

    Gave each item a parent list. modified database and views accordingly

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 '''UNIT TEST FILE'''
@@ -15,27 +15,43 @@ class HomePageTest(TestCase):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
 		
-	def test_home_page_returns_correct_html(self):
-		#setup
-		request = HttpRequest()
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
 		
-		#exercise
-		response = home_page(request)
-		expected_html = render_to_string('home.html')
+	# def test_home_page_returns_correct_html(self):
+		# #setup
+		# request = HttpRequest()
 		
-		#assert
-		self.assertEqual(response.content.decode(), expected_html)
+		# #exercise
+		# response = home_page(request)
+		# expected_html = render_to_string('home.html')
+		
+		# #assert
+		# self.assertEqual(response.content.decode(), expected_html)
 	
-class ItemModelTest(TestCase):
+	def test_home_uses_home_template(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
+	
+class ListAndItemModelTest(TestCase):
 	
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+	
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
 		first_item.save()
 		
 		second_item = Item()
 		second_item.text = "Item the second"
+		second_item.list = list_
 		second_item.save()
+		
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
 		
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -46,11 +62,16 @@ class ItemModelTest(TestCase):
 		self.assertEqual(first_saved_item.text, "The first (ever) list item")
 		self.assertEqual(second_saved_item.text, "Item the second")
 		
-class LiveViewTest(TestCase):
+		self.assertEqual(first_saved_item.list, list_)
+		self.assertEqual(second_saved_item.list, list_)
+		
+class ListViewTest(TestCase):
 	
 	def test_displays_all_list_items(self):
-		Item.objects.create(text="itemey 1")
-		Item.objects.create(text="itemey 2")
+		list_ = List.objects.create()
+	
+		Item.objects.create(text="itemey 1", list=list_)
+		Item.objects.create(text="itemey 2", list=list_)
 		
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		
